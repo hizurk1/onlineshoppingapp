634ab03beb1e36ca7200d40f43af6554a2ef7753
HongHai <supperamumu@gmail.com>
2022-04-17 16:11:02 +0700

    Lập trình trang Homepage: +Hiển thị danh sách sản phẩm "Dành cho bạn!" +Thêm thanh cuộn -Chưa cài đặt animation cho banner

--- app/src/main/java/com/android/onlineshoppingapp/fragments/HomePageFragment.java
+++ app/src/main/java/com/android/onlineshoppingapp/fragments/HomePageFragment.java
@@ -3,6 +3,7 @@ package com.android.onlineshoppingapp.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -80,17 +81,24 @@ public class HomePageFragment extends Fragment {
 
         View view   = inflater.inflate(R.layout.fragment_home_page,container,false);
 
-//        viewFlipper = view.findViewById(R.id.v_flipper);
+        //View flipper
+        viewFlipper = view.findViewById(R.id.v_flipper);
 
 
+
+        //Top item
         recyclerTopitem = (RecyclerView) view.findViewById(R.id.hgTopitem);
         recyclerTopitem.setAdapter(new RecyclerViewAdapterProduct(list));
         LinearLayoutManager layoutmangerTopitem = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
         recyclerTopitem.setLayoutManager(layoutmangerTopitem);
 
+
+
+        //Right item
         recyclerRightItem = (RecyclerView) view.findViewById(R.id.hgRighPrForYou);
-        LinearLayoutManager layoutmanagerRightitem = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
-        recyclerRightItem.setLayoutManager(layoutmanagerRightitem);
+        recyclerRightItem.setAdapter(new RecyclerViewAdapterProduct(list));
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),2);
+        recyclerRightItem.setLayoutManager(gridLayoutManager);
         return view;
     }
 
@@ -100,21 +108,28 @@ public class HomePageFragment extends Fragment {
         Product item2 = new Product(String.valueOf(2), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
         Product item3 = new Product(String.valueOf(3), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
         Product item4 = new Product(String.valueOf(4), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
+        Product item5 = new Product(String.valueOf(5), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
+        Product item6 = new Product(String.valueOf(6), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
+        Product item7 = new Product(String.valueOf(7), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
+        Product item8 = new Product(String.valueOf(8), "Xe mô hình", "String seller", "Vượt mọi trở ngại, thẳng tiến vạch đích!!!", 999, 5, 174, 23);
         list.add(item1);
         list.add(item2);
         list.add(item3);
         list.add(item4);
+        list.add(item5);
+        list.add(item6);
+        list.add(item7);
+        list.add(item8);
         return list;
     }
 
-//    public void flipperImages(int image){
-//        ImageView imageView = new ImageView(getActivity());
-//        imageView.setBackgroundResource(image);
-//
-//        viewFlipper.addView(imageView);
-//        viewFlipper.setFlipInterval(400);
-//        viewFlipper.setAutoStart(true);
-//
-//    }
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setBackgroundResource(image);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(400);
+        viewFlipper.setAutoStart(true);
+    }
 
 }
