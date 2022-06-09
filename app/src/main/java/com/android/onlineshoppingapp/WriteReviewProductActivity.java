f733f858aac36075045e4d9285f650240546f66a
heelbui <huuhieu992001@gmail.com>
2022-06-10 00:07:52 +0700

    Fix circle indicator

---
+++ app/src/main/java/com/android/onlineshoppingapp/WriteReviewProductActivity.java
@@ -0,0 +1,14 @@
+package com.android.onlineshoppingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteReviewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_review_product);
+    }
+}
