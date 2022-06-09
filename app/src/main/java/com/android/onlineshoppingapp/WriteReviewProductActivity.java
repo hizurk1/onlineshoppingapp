17225f1b9fe4f3c267b7d5d70ba9362a06d4e7b1
heelbui <huuhieu992001@gmail.com>
2022-06-10 01:04:52 +0700

    Add write a review page

--- app/src/main/java/com/android/onlineshoppingapp/WriteReviewProductActivity.java
+++ app/src/main/java/com/android/onlineshoppingapp/WriteReviewProductActivity.java
@@ -1,14 +1,49 @@
 package com.android.onlineshoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.android.onlineshoppingapp.adapters.WriteReviewAdapter;
+import com.android.onlineshoppingapp.models.Review;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteReviewProductActivity extends AppCompatActivity {
+
+    private RecyclerView rvWriteReview;
+    private ImageView ivBack;
+
+    private FirebaseAuth fAuth;
+    private FirebaseFirestore db;
+
+    private WriteReviewAdapter reviewAdapter;
+    private List<Review> reviewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review_product);
+
+        // init
+        rvWriteReview = findViewById(R.id.rvWriteReview);
+        ivBack = findViewById(R.id.ivBackWriteReview);
+
+        fAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        reviewList = new ArrayList<>();
+
+        reviewAdapter = new WriteReviewAdapter(reviewList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvWriteReview.setLayoutManager(linearLayoutManager);
+        rvWriteReview.setAdapter(reviewAdapter);
+
     }
 }
---
+++ app/src/main/java/com/android/onlineshoppingapp/adapters/WriteReviewAdapter.java
@@ -0,0 +1,84 @@
+package com.android.onlineshoppingapp.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.onlineshoppingapp.R;
+import com.android.onlineshoppingapp.models.Review;
+
+import java.util.List;
+
+public class WriteReviewAdapter extends RecyclerView.Adapter<WriteReviewAdapter.WriteReviewViewHolder> {
+
+    private List<Review> reviewList;
+
+    public WriteReviewAdapter(List<Review> reviewList) {
+        this.reviewList = reviewList;
+    }
+
+    @NonNull
+    @Override
+    public WriteReviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item_write_review, parent, false);
+        return new WriteReviewViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WriteReviewViewHolder holder, int position) {
+        Review review = reviewList.get(position);
+        if (review == null)
+            return;
+
+        String productName = "Tên sản phẩm"; // get product name
+        if (productName.length() > 50) {
+            holder.tvProductName.setText(String.format("%s...", productName.substring(0, 50)));
+        } else {
+            holder.tvProductName.setText(productName);
+        }
+
+        String category = "AAA"; // get category
+        holder.tvCategory.setText(String.format("Loại: %s", category));
+
+        holder.btnSubmit.setOnClickListener(view -> {
+            String content = holder.etWriteReview.getText().toString();
+            if (content.equals("")) {
+                Toast.makeText(view.getContext(), "Đánh giá không được để trống", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(view.getContext(), "Gửi đánh giá", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (reviewList.isEmpty())
+            return 0;
+        return reviewList.size();
+    }
+
+    class WriteReviewViewHolder extends RecyclerView.ViewHolder {
+
+        private Button btnSubmit;
+        private TextView tvProductName, tvCategory;
+        private EditText etWriteReview;
+
+        public WriteReviewViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvProductName = itemView.findViewById(R.id.tvProductNameReview);
+            tvCategory = itemView.findViewById(R.id.tvCategoryReview);
+            etWriteReview = itemView.findViewById(R.id.etWriteReview);
+            btnSubmit = itemView.findViewById(R.id.btnWriteReview);
+
+        }
+    }
+}
